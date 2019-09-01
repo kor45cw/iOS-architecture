6334c3f7ba9d435b53a29764035e46404c9596ea
kor45cw <kor45cw@gmail.com>
2019-09-01 18:16:13 +0900

    extension rootcomponent to main dependency

---
+++ Sample/Sample/Root/RootComponent+Main.swift
@@ -0,0 +1,14 @@
+//
+//  RootComponent+Main.swift
+//  Sample
+//
+//  Created by kor45cw on 01/09/2019.
+//  Copyright © 2019 kor45cw. All rights reserved.
+//
+
+import RIBs
+
+
+extension RootComponent: MainDependency {
+    
+}
